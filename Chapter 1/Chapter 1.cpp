3a05ba9f1e7e315cbea12882e9cae4e330f84aa6
CarlMatthewVerano <carlmatthewverano@gmail.com>
2024-09-05 15:13:50 +0800

    feat: IDE setup check if correct

--- Chapter 1/Chapter 1.cpp
+++ Chapter 1/Chapter 1.cpp
@@ -1,20 +1,59 @@
-// Chapter 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// This program prints the C++ language standard your compiler is currently using
+// Freely redistributable, courtesy of learncpp.com (https://www.learncpp.com/cpp-tutorial/what-language-standard-is-my-compiler-using/)
 
 #include <iostream>
 
-int main()
+const int numStandards = 7;
+// The C++26 stdCode is a placeholder since the exact code won't be determined until the standard is finalized
+const long stdCode[numStandards] = { 199711L, 201103L, 201402L, 201703L, 202002L, 202302L, 202612L };
+const char* stdName[numStandards] = { "Pre-C++11", "C++11", "C++14", "C++17", "C++20", "C++23", "C++26" };
+
+long getCPPStandard()
 {
-    std::cout << "Hello World!\n";
+    // Visual Studio is non-conforming in support for __cplusplus (unless you set a specific compiler flag, which you probably haven't)
+    // In Visual Studio 2015 or newer we can use _MSVC_LANG instead
+    // See https://devblogs.microsoft.com/cppblog/msvc-now-correctly-reports-__cplusplus/
+#if defined (_MSVC_LANG)
+    return _MSVC_LANG;
+#elif defined (_MSC_VER)
+    // If we're using an older version of Visual Studio, bail out
+    return -1;
+#else
+    // __cplusplus is the intended way to query the language standard code (as defined by the language standards)
+    return __cplusplus;
+#endif
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main()
+{
+    long standard = getCPPStandard();
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    if (standard == -1)
+    {
+        std::cout << "Error: Unable to determine your language standard.  Sorry.\n";
+        return 0;
+    }
+
+    for (int i = 0; i < numStandards; ++i)
+    {
+        // If the reported version is one of the finalized standard codes
+        // then we know exactly what version the compiler is running
+        if (standard == stdCode[i])
+        {
+            std::cout << "Your compiler is using " << stdName[i]
+                << " (language standard code " << standard << "L)\n";
+            break;
+        }
+
+        // If the reported version is between two finalized standard codes,
+        // this must be a preview / experimental support for the next upcoming version.
+        if (standard < stdCode[i])
+        {
+            std::cout << "Your compiler is using a preview/pre-release of " << stdName[i]
+                << " (language standard code " << standard << "L)\n";
+            break;
+        }
+    }
+
+    return 0;
+}
